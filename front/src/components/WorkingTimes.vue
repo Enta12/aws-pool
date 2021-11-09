14d14d037be92f8473142c81d1f15e1e9130b9cf
florent <florent.tetia@epitech.eu>
2021-11-09 15:43:02 +0100

    [FIX]clean workingtimes

--- front/src/components/WorkingTimes.vue
+++ front/src/components/WorkingTimes.vue
@@ -2,81 +2,80 @@
   <div class="working-times">
     <v-card elevation="16" class="mx-auto">
       <span class="text-center">
-      <strong><h2>WorkingTimes list</h2></strong>
+        <strong><h2>WorkingTimes list</h2></strong>
       </span>
       <v-list class="listCard">
-         <!-- <template>
-                <v-expansion-panels>
-                  <v-expansion-panel
-                    v-for="(item,i) in 12"
-                    :key="i"
-                  >
-                    <v-expansion-panel-header >
-                      {{months[i]}}
-                    </v-expansion-panel-header>
-                    <v-expansion-panel-content  v-if="month[i] == test" >
-                      <v-list-item v-for="i in test" :key="i">
-
-                      </v-list-item>
-                     {{i}}
-
-                    </v-expansion-panel-content>
-                  </v-expansion-panel>
-                </v-expansion-panels>
-            </template> -->
-              <template>
-                <v-expansion-panels focusable  multiple>
-                  <v-expansion-panel
-                    v-for="(item,i) in 12"
-                    :key="i"
-                    v-model="panel"
-                   
-                  >
-                    <v-expansion-panel-header class="headpanel ">
-                     <span class='text-center'> <h2>{{months[i]}}</h2></span>
-                    </v-expansion-panel-header>
-                    <v-expansion-panel-content class='justify-center' >
-                      
-                      
-                    <v-expansion-panel
-                    v-for="(item,itm) in 7"
-                    :key="itm"
-                   
-                    multiple
+        <template>
+          <v-expansion-panels focusable multiple>
+            <v-expansion-panel v-for="(item, i) in 12" :key="i" v-model="panel">
+              <v-expansion-panel-header class="headpanel">
+                <span class="text-center">
+                  <h2>{{ months[i] }}</h2></span
+                >
+              </v-expansion-panel-header>
+              <v-expansion-panel-content class="justify-center">
+                <v-expansion-panel v-for="(item, itm) in 7" :key="itm" multiple>
+                  <v-expansion-panel-header class="headpanelbis">
+                    <span class="text-center">
+                      <h3>{{ days[itm] }}</h3></span
                     >
-                     <v-expansion-panel-header class="headpanelbis ">
-                     <span class='text-center'> <h3>{{days[itm]}}</h3></span>
-                    </v-expansion-panel-header>
-                       <v-expansion-panel-content class='justify-center' >
-                          <v-list >
-                         <v-list-item v-for="it in ttest" :key="it" class='justify-center' >
-                           <v-card v-if="months[i] == it['monthName'] && days[itm] == it['dayName']" class='cardWorks  ' elevation='12'>
-                             <div class='cardWorkst d-inline-flex'>
-                          <span class="flex-row "> 
-                          <v-card-text> <v-card-subtitle > <h3> {{ it['dayName']}} {{ it['worksdate'][0]['start'][0]}}</h3> </v-card-subtitle></v-card-text></span>
-                            <span class=" flex-row">
-                           <v-card-text> <v-card-subtitle>Start at : {{it['worksdate'][0]['start'][1]}}</v-card-subtitle></v-card-text>
-                            </span>  
-                            <span class=" flex-row ">
-                           <v-card-text>  <v-card-subtitle>End at :  {{it['worksdate'][1]['end'][1]}}</v-card-subtitle> </v-card-text>
+                  </v-expansion-panel-header>
+                  <v-expansion-panel-content class="justify-center">
+                    <v-list>
+                      <v-list-item
+                        v-for="it in ttest"
+                        :key="it"
+                        class="justify-center"
+                      >
+                        <v-card
+                          v-if="
+                            months[i] == it['monthName'] &&
+                            days[itm] == it['dayName']
+                          "
+                          class="cardWorks"
+                          elevation="12"
+                        >
+                          <div class="cardWorkst d-inline-flex">
+                            <span class="flex-row">
+                              <v-card-text>
+                                <v-card-subtitle>
+                                  <h3>
+                                    {{ it["dayName"] }}
+                                    {{ it["worksdate"][0]["start"][0] }}
+                                  </h3>
+                                </v-card-subtitle></v-card-text
+                              ></span
+                            >
+                            <span class="flex-row">
+                              <v-card-text>
+                                <v-card-subtitle
+                                  >Start at :
+                                  {{
+                                    it["worksdate"][0]["start"][1]
+                                  }}</v-card-subtitle
+                                ></v-card-text
+                              >
                             </span>
-                             </div>
-                           </v-card>
-   
+                            <span class="flex-row">
+                              <v-card-text>
+                                <v-card-subtitle
+                                  >End at :
+                                  {{
+                                    it["worksdate"][1]["end"][1]
+                                  }}</v-card-subtitle
+                                >
+                              </v-card-text>
+                            </span>
+                          </div>
+                        </v-card>
                       </v-list-item>
-                      
-                      </v-list>
-                       </v-expansion-panel-content>
-                    </v-expansion-panel>  
-                     
-                     
-
-
-                    </v-expansion-panel-content>
-                  </v-expansion-panel>
-                </v-expansion-panels>
-            </template>
-      
+                    </v-list>
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </template>
       </v-list>
 
       <v-virtual-scroll
@@ -84,148 +83,112 @@
         class="mycard"
         item-height="45"
       >
-        <!-- <template v-slot:default="{ item }" v-if = "responseAvailable == true" >
-          
-          <v-list-item  :key="item" >
-
-            <v-list-item-action>
-              <v-btn
-                  fab
-                  small
-                  depressed
-                  color="primary"
-              >
-               {{ item }}
-              </v-btn>
-            </v-list-item-action>
-
-            <v-list-item-content>
-              <v-list-item-title>
-                User Database Record <strong>ID {{ item.id }}</strong>
-              </v-list-item-title>
-            </v-list-item-content>
-
-            <v-list-item-action>
-              <v-icon small>
-                mdi-open-in-new
-              </v-icon>
-            </v-list-item-action>
-          </v-list-item>
-        
-         
-          <v-divider></v-divider>
-        </template>-->
       </v-virtual-scroll>
     </v-card>
   </div>
 </template>
 
 <script>
-// const iduser =this.$store.state.user.id;
 import axios from "axios";
 import moment from "moment";
-import Vue from 'vue';
+import Vue from "vue";
 
 export default {
   name: "WorkingTimes",
   data() {
-    return { data_workingtimes: {},test:[],ttest:[],worksdays:[], nbWorks:0,day: [], month: [],months:['January','February','March','April','May','June','July','August','September','October','November','December'],days:['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'], counter: "" };
+    return {
+      data_workingtimes: {},
+      test: [],
+      ttest: [],
+      worksdays: [],
+      nbWorks: 0,
+      day: [],
+      month: [],
+      months: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+      days: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
+      counter: "",
+    };
   },
   async created() {
-    
-    this.GetWorkingTImes()
-     
+    this.GetWorkingTImes();
   },
 
   methods: {
-    
-   
-    pushWorks(month,works,day){
-      
-      this.ttest.push({monthName:month,worksdate:works,dayName:day})
-     
+    pushWorks(month, works, day) {
+      this.ttest.push({ monthName: month, worksdate: works, dayName: day });
     },
-  async  GetWorkingTImes(){
+    async GetWorkingTImes() {
+      axios
+        .get(
+          "http://localhost:4000/api/workingtimes/" + this.$store.state.user.id
+        )
+        .then((res) => {
+          this.data_workingtimes = res.data.data;
 
-       axios
-      .get(
-        "http://localhost:4000/api/workingtimes/" + this.$store.state.user.id
-      )
-      .then((res) => {
+          res.data.data.forEach((elmt) => {
+            let getdaystart = elmt.start.split("T");
+            let getdayEnd = elmt.end.split("T");
+            let date_Formated = moment(getdaystart[0]).format("dddd MMMM YYYY");
+            let month_date = date_Formated.split(" ");
+            this.worksdays.push({ start: getdaystart });
+            this.worksdays.push({ end: getdayEnd });
+            this.worksdays.push({ day: month_date });
+            this.worksdays.push(elmt.id);
+            let currentday = month_date[0];
+            this.nbWorks++;
+            this.pushWorks(month_date[1], this.worksdays, currentday);
+            this.worksdays = [];
+            this.test.push(month_date[1], getdaystart[0]);
+            this.test.push(getdaystart[0]);
+          });
 
-        this.data_workingtimes = res.data.data;
-
-      
-            res.data.data.forEach(elmt =>{
-              // let splitdaystart= moment(elmt.start.format("dddd-MMMM-YY:H:M:A"))
-            // let  splitendstart =moment(elmt.end.format("A"))
-              let getdaystart = elmt.start.split('T')
-              let getdayEnd = elmt.end.split('T')
-              let date_Formated = moment(getdaystart[0]).format("dddd MMMM YYYY");
-              //  let hour_Formated = moment(getdaystart[1]).format("hh:mm A");
-
-              let month_date = date_Formated.split(" ");
-          
+            let test = this.data_workingtimes.map((data) => {
+            let str_date = data.start.split("T");
+            let dateFormated = moment(str_date[0]).format("dddd MMMM YY");
+            let getday = dateFormated.split(" ");
+            this.day.push(getday[0]);
          
 
-              this.worksdays.push({'start':getdaystart})
-              this.worksdays.push({'end':getdayEnd})
-            
-              this.worksdays.push({'day':month_date})
-              this.worksdays.push(elmt.id)
-              let currentday =month_date[0]
-              this.nbWorks++
-              this.pushWorks(month_date[1],this.worksdays,currentday)
-              this.worksdays=[]
-
-              this.test.push(month_date[1],getdaystart[0])
-              this.test.push(getdaystart[0])
-
-
-             
-            })
-                        
-
-        let test = this.data_workingtimes.map((data) => 
-        {
-          //  let lastdate= moment(data.start).format('dddd/mm/YY')
-          let str_date = data.start.split("T");
-          
-          let dateFormated = moment(str_date[0]).format("dddd MMMM YY");
-
-          let getday = dateFormated.split(" ");
-
-          this.day.push(getday[0]);
-        //     res.data.data.forEach((value, index) => {
-        //     // arr.push(value);
-        //     // this.allDatas.push(value)
-        //     console.log('foreach val',value);
-        //     // console.log('test get month',this.allDatas)
-        //     console.log('foreach index',index);
-        // });
-        
-
-         this.month.push(getday[1]);
-          this.data_formated = dateFormated;
+            this.month.push(getday[1]);
+            this.data_formated = dateFormated;
+          });
+          return test;
+        })
+        .catch((e) => {
+          console.error(e);
         });
-        return test
-       
-      })
-      .catch((e) => {
-        console.error(e);
-      });
     },
-    showWorkingTimes(){
+    showWorkingTimes() {
       let demo = new Vue({
-        el:'#demof',
-        data:{
-          parentMsg:'YO',
-          items:this.ttest
-        }
-
-      })
-      return demo
-    }
+        el: "#demof",
+        data: {
+          parentMsg: "YO",
+          items: this.ttest,
+        },
+      });
+      return demo;
+    },
   },
 };
 </script>
@@ -233,14 +196,10 @@ export default {
 <style scoped>
 .mycard {
   height: 65vh;
-  /* justify-content: center; */
 }
 .listCard {
   height: 35vh;
-  /* justify-content: center; */
-  /* align-items: center; */
-  /* border:solid black 2;
-  border-color: black; */
+ 
 }
 .itemCard {
   background-color: cornflowerblue;
@@ -248,7 +207,7 @@ export default {
   width: 85%;
   border: 3;
   margin: 5%;
-  /* margin-left: 10%; */
+ 
 
   border-radius: 20;
 }
@@ -256,7 +215,7 @@ export default {
   width: 90%;
   height: 8%;
   flex-direction: row;
-  margin:1%
+  margin: 1%;
 }
 .cardWorkst {
   width: 100%;
@@ -270,13 +229,12 @@ export default {
 .datebox {
   background-color: crimson;
   height: 10vh;
-  /* flex-direction: row; */
-  /* justify-content: space-between; */
+  
 }
 .startdate {
   font-size: 20;
   background-color: yellow;
-  /* flex: 2; */
+
 }
 .enddate {
   font-size: 20;
