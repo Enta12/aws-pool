b4fb574ac37cd37a9dd009ee67ddaa914871298c
florent <florent.tetia@epitech.eu>
2021-11-12 17:03:25 +0100

    [FIX] workingtimes calendar

--- front/src/components/WorkingTimes.vue
+++ front/src/components/WorkingTimes.vue
@@ -1,90 +1,117 @@
 <template>
   <div class="working-times">
-    <v-card elevation="16" class="mx-auto">
-      <span class="text-center">
-        <strong><h2>WorkingTimes list</h2></strong>
-      </span>
-      <v-list class="listCard">
+ 
+    <v-list class="listCard">
+      <div>
+        <v-toolbar flat>
+          <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+            Today
+          </v-btn>
+          <v-btn fab text small color="grey darken-2" @click="prev">
+            <v-icon small> mdi-chevron-left </v-icon>
+          </v-btn>
+          <v-btn fab text small color="grey darken-2" @click="next">
+            <v-icon small> mdi-chevron-right </v-icon>
+          </v-btn>
+          <v-toolbar-title v-if="$refs.calendar">
+            {{ $refs.calendar.title }}
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-menu bottom right>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
+                <span>{{ typeToLabel[type] }}</span>
+                <v-icon right> mdi-menu-down </v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="type = 'day'">
+                <v-list-item-title>Day</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'week'">
+                <v-list-item-title>Week</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'month'">
+                <v-list-item-title>Month</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = '4day'">
+                <v-list-item-title>4 days</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar>
         <template>
-          <v-expansion-panels focusable multiple>
-            <v-expansion-panel v-for="(item, i) in 12" :key="i" v-model="panel">
-              <v-expansion-panel-header class="headpanel">
-                <span class="text-center">
-                  <h2>{{ months[i] }}</h2></span
-                >
-              </v-expansion-panel-header>
-              <v-expansion-panel-content class="justify-center">
-                <v-expansion-panel v-for="(item, itm) in 7" :key="itm" multiple>
-                  <v-expansion-panel-header class="headpanelbis">
-                    <span class="text-center">
-                      <h3>{{ days[itm] }}</h3></span
-                    >
-                  </v-expansion-panel-header>
-                  <v-expansion-panel-content class="justify-center">
-                    <v-list>
-                      <v-list-item
-                        v-for="it in ttest"
-                        :key="it"
-                        class="justify-center"
-                      >
-                        <v-card
-                          v-if="
-                            months[i] == it['monthName'] &&
-                            days[itm] == it['dayName']
-                          "
-                          class="cardWorks"
-                          elevation="12"
-                        >
-                          <div class="cardWorkst d-inline-flex">
-                            <span class="flex-row">
-                              <v-card-text>
-                                <v-card-subtitle>
-                                  <h3>
-                                    {{ it["dayName"] }}
-                                    {{ it["worksdate"][0]["start"][0] }}
-                                  </h3>
-                                </v-card-subtitle></v-card-text
-                              ></span
-                            >
-                            <span class="flex-row">
-                              <v-card-text>
-                                <v-card-subtitle
-                                  >Start at :
-                                  {{
-                                    it["worksdate"][0]["start"][1]
-                                  }}</v-card-subtitle
-                                ></v-card-text
-                              >
-                            </span>
-                            <span class="flex-row">
-                              <v-card-text>
-                                <v-card-subtitle
-                                  >End at :
-                                  {{
-                                    it["worksdate"][1]["end"][1]
-                                  }}</v-card-subtitle
-                                >
-                              </v-card-text>
-                            </span>
-                          </div>
-                        </v-card>
-                      </v-list-item>
-                    </v-list>
-                  </v-expansion-panel-content>
-                </v-expansion-panel>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-expansion-panels>
-        </template>
-      </v-list>
+        <v-calendar
+          @click:more="viewDay"
+          @click:date="viewDay"
+          ref="calendar"
+          v-model="focus"
+          color="primary"
+          :events="dataWorks"
+          :event-color="getEventColor"
+          :type="type"
+          @click:event="showEvent"
+          @change="updateRange"
+        >
 
-      <v-virtual-scroll
-        :items="data_workingtimes"
-        class="mycard"
-        item-height="45"
-      >
-      </v-virtual-scroll>
-    </v-card>
+        
+        </v-calendar>
+ </template>
+  <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          offset-x
+        >
+          <v-card
+            color="grey lighten-4"
+            min-width="350px"
+            flat
+          >
+            <v-toolbar
+              :color="selectedEvent.color"
+              dark
+            >
+              <v-btn icon>
+                <v-icon>mdi-pencil</v-icon>
+                
+              </v-btn>
+
+              <v-toolbar-title v-html="selectedEvent.id_works"></v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+           
+            </v-toolbar>
+            <div class="d-flex justify-center">
+              <v-card-text class="justify-center align-center">
+              
+              <h3>Id : {{selectedEvent.id}}</h3>
+            </v-card-text>
+           <v-card-text class="justify-center align-center">
+          
+              <h3 > start : {{selectedEvent.start}}</h3>
+            </v-card-text>
+              <v-card-text>
+             
+              <h3> End : {{selectedEvent.end}} </h3>
+            </v-card-text>
+            </div>
+            <v-card-actions>
+              <v-btn
+                text
+                color="secondary"
+                @click="selectedOpen = false"
+              >
+                Cancel
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
+        
+      </div>
+    </v-list>
   </div>
 </template>
 
@@ -101,7 +128,9 @@ export default {
       test: [],
       ttest: [],
       worksdays: [],
-      nbWorks: 0,
+      yearSelectedCheck: false,
+      yearSelected: "",
+      daySelected: "",
       day: [],
       month: [],
       months: [
@@ -119,6 +148,7 @@ export default {
         "December",
       ],
       days: [
+        " ",
         "Monday",
         "Tuesday",
         "Wednesday",
@@ -127,16 +157,58 @@ export default {
         "Saturday",
         "Sunday",
       ],
-      counter: "",
+      years: [" "],
+      focus: "",
+      type: "month",
+      typeToLabel: {
+        month: "Month",
+        week: "Week",
+        day: "Day",
+        "4day": "4 Days",
+      },
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false,
+      dataWorks:[],
+      events: [],
+      colors: [
+        "blue",
+        "indigo",
+        "deep-purple",
+        "cyan",
+        "green",
+        "orange",
+        "grey darken-1",
+      ],
     };
   },
-  async created() {
+ 
+  created() {
+
+  this.dataWorks.forEach((i)=>{
+    console.log('I',i)
+  })
+
     this.GetWorkingTImes();
+    this.getYears();
   },
 
   methods: {
     pushWorks(month, works, day) {
-      this.ttest.push({ monthName: month, worksdate: works, dayName: day });
+      let Color = this.colors[this.rnd(0, this.colors.length - 1)];
+      this.ttest.push({
+        monthName: month,
+        worksdate: works,
+        dayName: day,
+        color: Color,
+      });
+    },
+    getYears() {
+      let currentYear = new Date().getFullYear();
+      let min = currentYear - 5;
+      for (let i = currentYear; i >= min; i--) {
+        this.years.push(i);
+      }
     },
     async GetWorkingTImes() {
       axios
@@ -149,26 +221,47 @@ export default {
           res.data.data.forEach((elmt) => {
             let getdaystart = elmt.start.split("T");
             let getdayEnd = elmt.end.split("T");
-            let date_Formated = moment(getdaystart[0]).format("dddd MMMM YYYY");
+            let date_Formated = moment(getdaystart[0]).format("YYYY-MM-DD");
+            let date_FormatedEnd = moment(getdayEnd[0]).format( "YYYY-MM-DD");
+            
+            let strStart=date_Formated+' '+getdaystart[1]
+            let strEnd=date_FormatedEnd+' '+getdayEnd[1]
+            const allDay = this.rnd(0, 3) === 0;
+            this.dataWorks.push({start:strStart,end:strEnd,color:this.colors[this.rnd(0, this.colors.length - 1)],timed:!allDay,id:elmt.id})
+
             let month_date = date_Formated.split(" ");
-            this.worksdays.push({ start: getdaystart });
-            this.worksdays.push({ end: getdayEnd });
+            let month_date_end = date_FormatedEnd.split(" ");
+
+            let startDay = month_date[0];
+            let endDay = month_date_end[0];
+
+            this.worksdays.push({ start: elmt.start });
+            this.worksdays.push({ end: elmt.end });
+
             this.worksdays.push({ day: month_date });
+            this.worksdays.push({ dayStart: startDay });
+            this.worksdays.push({ dayEnd: endDay });
+
+            this.worksdays.push({ yearEnd: month_date_end[2] });
+            this.worksdays.push({ yearStart: month_date[2] });
+
             this.worksdays.push(elmt.id);
+
             let currentday = month_date[0];
             this.nbWorks++;
             this.pushWorks(month_date[1], this.worksdays, currentday);
             this.worksdays = [];
-            this.test.push(month_date[1], getdaystart[0]);
-            this.test.push(getdaystart[0]);
+
           });
 
             let test = this.data_workingtimes.map((data) => {
             let str_date = data.start.split("T");
+
             let dateFormated = moment(str_date[0]).format("dddd MMMM YY");
+
             let getday = dateFormated.split(" ");
+
             this.day.push(getday[0]);
-         
 
             this.month.push(getday[1]);
             this.data_formated = dateFormated;
@@ -189,11 +282,92 @@ export default {
       });
       return demo;
     },
+
+    selectDay(d) {
+      this.daySelected = d;
+    },
+    selectYear(y) {
+      this.yearSelected = y;
+    },
+    setToday() {
+      this.focus = "";
+      console.log(this.focus);
+    },
+    getEventColor(event) {
+      return event.color;
+    },
+    viewDay({ date }) {
+      this.focus = date;
+    },
+    prev() {
+      this.$refs.calendar.prev();
+    },
+    next() {
+      this.$refs.calendar.next();
+    },
+    showEvent({ nativeEvent, event }) {
+      
+      const open = () => {
+        this.selectedEvent = event;
+        this.selectedElement = nativeEvent.target;
+        requestAnimationFrame(() =>
+          requestAnimationFrame(() => (this.selectedOpen = true))
+        );
+      };
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false;
+        requestAnimationFrame(() => requestAnimationFrame(() => open()));
+      } else {
+        open();
+      }
+
+      nativeEvent.stopPropagation();
+    },
+    updateRange() {
+      const events = [];
+
+     
+      this.dataWorks.forEach( (el) => {
+      const allDay = this.rnd(0, 3) === 0
+      let startTime=el.start
+      let endTime = el.end
+      events.push({
+        start: startTime,
+        end: endTime,
+        color:this.colors[this.rnd(0, this.colors.length - 1)],
+        timed: !allDay,
+        id:el.id
+      })
+      })
+
+
+      this.events = events;
+    },
+    rnd(a, b) {
+      return Math.floor((b - a + 1) * Math.random()) + a;
+    },
+     
+  },
+  computed: {
+    eventsMap () {
+        const map = {}
+        this.dataWorks.forEach(e => (map[e['worksdate']] = map[e['worksdate']] || []).push(e))
+
+        return map
+      }
+  },
+  mounted() {
+  
+    this.$refs.calendar.checkChange();
   },
 };
 </script>
 
 <style scoped>
+.working-times {
+  height: 100%;
+}
 .mycard {
   height: 65vh;
 }
@@ -210,6 +384,9 @@ export default {
  
 
   border-radius: 20;
+}
+.btnYear {
+  margin: 2%;
 }
 .cardWorks {
   width: 90%;
@@ -234,7 +411,7 @@ export default {
 .startdate {
   font-size: 20;
   background-color: yellow;
-
+ 
 }
 .enddate {
   font-size: 20;
@@ -245,5 +422,21 @@ export default {
   justify-content: center;
   text-align: center;
   background-color: rgb(37, 209, 157);
+}
+.yearsbox {
+  height: 15%;
+  margin-top: 0.5%;
+}
+.teest {
+  background-color: blueviolet;
+}
+.selectDay {
+  width: 8%;
+  margin-right: 1%;
+  margin-left: 1%;
+}
+.selectYear {
+  width: 10%;
+  margin-right: 1%;
 }
 </style>
